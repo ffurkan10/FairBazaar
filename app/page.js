fdb2a23312aa6102ca3740029c9b58b797b64a79
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2025-10-03 17:49:13 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,11 +2,15 @@ import ProductContent from '@/components/home/ProductContent';
 import HomeHeroSlider from '@/components/sliders/HomeHeroSlider';
 import Spinner from '@/components/spinner';
 import { getProducts } from '@/lib/api';
+import GetServerSide from '@/lib/getServerSide';
 import React from 'react'
 
 const Home = async () => {
 
-  const {data} = await GetServerSide(`https://localhost:3001/products`);
+  const data = await GetServerSide(`http://localhost:3001/products`);
+
+  console.log(data);
+  
 
   if(!data) {
     return <div className='container pt-10'><Spinner /></div>
